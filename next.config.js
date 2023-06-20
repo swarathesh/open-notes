eb718c84a2155ce7f883558e463418dadc105878
chandra swarathesh addanki <swarathesh60@gmail.com>
2023-06-20 19:28:16 -0400

    no cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { images: {
-    domains: ['lh3.googleusercontent.com'],
-  }}
+    domains: ['lh3.googleusercontent.com']
+  },
+    source: '/api/prompt',
+    headers: [
+      {
+        key: 'Cache-Control',
+        value: 'no-store, max-age=0',
+      },
+    ]
+  }
 
+  
 module.exports = nextConfig
